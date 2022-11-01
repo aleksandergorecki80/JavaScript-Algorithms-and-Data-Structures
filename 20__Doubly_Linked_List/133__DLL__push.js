87546a568cc8bd0799bab6dcdab8f512b2e054dc
Aleksander <aleksandergorecki80@gmail.com>
2022-11-01 19:01:21 +0100

    Doubly Linked List - push and pop

---
+++ 20__Doubly_Linked_List/133__DLL__push.js
@@ -0,0 +1,35 @@
+class Node {
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+const list = new DoublyLinkedList();
+list.push(6)
+list.push(7)
+list.push(8)
+
+console.log(list)
